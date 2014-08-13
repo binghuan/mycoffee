76546c06bdac091b3a53bc8d5f4abae9f41ba31d
Powen Tan <powentan0330@gmail.com>
2014-08-13 23:46:57 +0800

    Adjust default nearby distance

--- dataList.js
+++ dataList.js
@@ -1,103 +1,124 @@
 var dataList = [
     {
-        name: 'Xiaomijo小米酒咖啡',
-        address: '台北市永康街41巷5號',
-        phone: '(02)2395-7617',
-        openTime: '二-日1100-2300'
+        "name": "Xiaomijo小米酒咖啡",
+        "address": "台北市永康街41巷5號",
+        "phone": "(02)2395-7617",
+        "openTime": "二-日1100-2300",
+        "latitude": 25.0305179,
+        "longitude": 121.52972
     },
     {
-        name: 'Caffé Libero 小自由咖啡館',
-        address: '台北市金華街243巷1號',
-        phone: '(02)2356-7129',
-        openTime: '一至六1200-2400。日1200-1800'
-    },
-
-    {
-        name: 'YABOO Cafe‧鴉埠咖啡',
-        address: '台北市永康街41巷26號',
-        phone: '(02)2391-2868',
-        openTime: '1300~0100 (一休)'
+        "name": "Caffé Libero 小自由咖啡館",
+        "address": "台北市金華街243巷1號",
+        "phone": "(02)2356-7129",
+        "openTime": "一至六1200-2400。日1200-1800",
+        "latitude": 25.0300251,
+        "longitude": 121.530619
     },
     {
-        name: 'café flat white',
-        address: '台北市永康街41巷12號',
-        phone: '(02)3393-8872',
-        openTime: '1100–2300(二休)'
+        "name": "YABOO Cafe‧鴉埠咖啡",
+        "address": "台北市永康街41巷26號",
+        "phone": "(02)2391-2868",
+        "openTime": "1300~0100 (一休)",
+        "latitude": 25.030422,
+        "longitude": 121.530585
     },
     {
-        name: '閑隅 T.Loafer',
-        address: '台北市大安區金山南路2段141巷20號',
-        phone: '',
-        openTime: '1200-2400'
+        "name": "café flat white",
+        "address": "台北市永康街41巷12號",
+        "phone": "(02)3393-8872",
+        "openTime": "1100–2300(二休)",
+        "latitude": 25.0304039,
+        "longitude": 121.53002
     },
     {
-        name: '卡瓦利義式咖啡',
-        address: '台北市永康街2巷5號',
-        phone: '(02)2394-7516',
-        openTime: '日~四1130-2300。五六1130-2400'
+        "name": "閑隅 T.Loafer",
+        "address": "台北市大安區金山南路2段141巷20號",
+        "phone": "",
+        "openTime": "1200-2400",
+        "latitude": 25.029445,
+        "longitude": 121.527556
     },
     {
-        name: 'Ecole Cafe 學校咖啡館',
-        address: '台北市青田街一巷六號',
-        phone: '(02)2322-2725',
-        openTime: 'Sun-Thu 13:00~23:00 / Fri 13:00~01:00 / Sat 11:00~01:00'
+        "name": "卡瓦利義式咖啡",
+        "address": "台北市永康街2巷5號",
+        "phone": "(02)2394-7516",
+        "openTime": "日~四1130-2300。五六1130-2400",
+        "latitude": 25.0332769,
+        "longitude": 121.529489
     },
     {
-        name: '咖啡黑潮',
-        address: '台北市和平東路一段141巷9-1號',
-        phone: '(02)2357-0018',
-        openTime: '1400-2400(一休)'
+        "name": "Ecole Cafe 學校咖啡館",
+        "address": "台北市青田街一巷六號",
+        "phone": "(02)2322-2725",
+        "openTime": "Sun-Thu 13:00~23:00 / Fri 13:00~01:00 / Sat 11:00~01:00",
+        "latitude": 25.0289119,
+        "longitude": 121.532172
     },
     {
-        name: 'COZY Cafe 咖啡館',
-        address: '台北市麗水街33巷23號',
-        phone: '(02)2397-4754',
-        openTime: '1000-2400'
+        "name": "咖啡黑潮",
+        "address": "台北市和平東路一段141巷9-1號",
+        "phone": "(02)2357-0018",
+        "openTime": "1400-2400(一休)",
+        "latitude": 25.027795,
+        "longitude": 121.530646
     },
     {
-        name: '靜香',
-        address: '台北市麗水街24-1號',
-        phone: '(02)2396-6381',
-        openTime: '1430~2130(四休)'
+        "name": "COZY Cafe 咖啡館",
+        "address": "台北市麗水街33巷23號",
+        "phone": "(02)2397-4754",
+        "openTime": "1000-2400",
+        "latitude": 25.028039,
+        "longitude": 121.530065
     },
     {
-        name: '茴香',
-        address: '台北市永康街37巷16號',
-        phone: '02 2394-2525',
-        openTime: '下午三點到晚上一點'
+        "name": "靜香",
+        "address": "台北市麗水街24-1號",
+        "phone": "(02)2396-6381",
+        "openTime": "1430~2130(四休)",
+        "latitude": 25.029603,
+        "longitude": 121.528407
     },
     {
-        name: '',
-        address: '',
-        phone: '',
-        openTime: ''
+        "name": "茴香",
+        "address": "台北市永康街37巷16號",
+        "phone": "02 2394-2525",
+        "openTime": "下午三點到晚上一點",
+        "latitude": 25.030704,
+        "longitude": 121.530066
     },
     {
-        name: '',
-        address: '',
-        phone: '',
-        openTime: ''
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
     },
     {
-        name: '',
-        address: '',
-        phone: '',
-        openTime: ''
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
     },
     {
-        name: '',
-        address: '',
-        phone: '',
-        openTime: ''
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
     },
     {
-        name: '',
-        address: '',
-        phone: '',
-        openTime: ''
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
     },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    }
 ];
 
-if(module.exports != null) {
+if(typeof module !== 'undefined' && module != null && module.exports != null) {
     module.exports = dataList;
 }
