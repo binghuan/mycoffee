db74e926c8b883d5db7136637121932cd039cf5c
Powen Tan <powentan0330@gmail.com>
2014-08-13 16:37:27 +0800

    Reformat dataList.js and add some coffee stores

--- dataList.js
+++ dataList.js
@@ -1,3 +1,103 @@
 var dataList = [
-"25.0444393157959|121.53237152099609|1|台北市|中正區|人人素食|02-2341-3365|台北市中正區八德路一段83巷9弄49號|紅燒湯,魯香飯,紅麴素雞丁|無|0|無|無|121.7817407|25.04357|素食|麵條類|0|無|2.990000009536743|55|402|177|13"
+    {
+        name: 'Xiaomijo小米酒咖啡',
+        address: '台北市永康街41巷5號',
+        phone: '(02)2395-7617',
+        openTime: '二-日1100-2300'
+    },
+    {
+        name: 'Caffé Libero 小自由咖啡館',
+        address: '台北市金華街243巷1號',
+        phone: '(02)2356-7129',
+        openTime: '一至六1200-2400。日1200-1800'
+    },
+
+    {
+        name: 'YABOO Cafe‧鴉埠咖啡',
+        address: '台北市永康街41巷26號',
+        phone: '(02)2391-2868',
+        openTime: '1300~0100 (一休)'
+    },
+    {
+        name: 'café flat white',
+        address: '台北市永康街41巷12號',
+        phone: '(02)3393-8872',
+        openTime: '1100–2300(二休)'
+    },
+    {
+        name: '閑隅 T.Loafer',
+        address: '台北市大安區金山南路2段141巷20號',
+        phone: '',
+        openTime: '1200-2400'
+    },
+    {
+        name: '卡瓦利義式咖啡',
+        address: '台北市永康街2巷5號',
+        phone: '(02)2394-7516',
+        openTime: '日~四1130-2300。五六1130-2400'
+    },
+    {
+        name: 'Ecole Cafe 學校咖啡館',
+        address: '台北市青田街一巷六號',
+        phone: '(02)2322-2725',
+        openTime: 'Sun-Thu 13:00~23:00 / Fri 13:00~01:00 / Sat 11:00~01:00'
+    },
+    {
+        name: '咖啡黑潮',
+        address: '台北市和平東路一段141巷9-1號',
+        phone: '(02)2357-0018',
+        openTime: '1400-2400(一休)'
+    },
+    {
+        name: 'COZY Cafe 咖啡館',
+        address: '台北市麗水街33巷23號',
+        phone: '(02)2397-4754',
+        openTime: '1000-2400'
+    },
+    {
+        name: '靜香',
+        address: '台北市麗水街24-1號',
+        phone: '(02)2396-6381',
+        openTime: '1430~2130(四休)'
+    },
+    {
+        name: '茴香',
+        address: '台北市永康街37巷16號',
+        phone: '02 2394-2525',
+        openTime: '下午三點到晚上一點'
+    },
+    {
+        name: '',
+        address: '',
+        phone: '',
+        openTime: ''
+    },
+    {
+        name: '',
+        address: '',
+        phone: '',
+        openTime: ''
+    },
+    {
+        name: '',
+        address: '',
+        phone: '',
+        openTime: ''
+    },
+    {
+        name: '',
+        address: '',
+        phone: '',
+        openTime: ''
+    },
+    {
+        name: '',
+        address: '',
+        phone: '',
+        openTime: ''
+    },
 ];
+
+if(module.exports != null) {
+    module.exports = dataList;
+}
