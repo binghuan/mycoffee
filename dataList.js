47fc6d2cabacc681c2478a61126e8e3a366f457e
Powen Tan <powentan0330@gmail.com>
2014-08-14 14:03:42 +0800

    Add more coffee stores

--- dataList.js
+++ dataList.js
@@ -88,6 +88,336 @@ var dataList = [
         "longitude": 121.530066
     },
     {
+        "name": "品客咖啡屋",
+        "address": "雲和街48之1號",
+        "phone": "02 2369 1264",
+        "openTime": "",
+        "latitude": 25.0234579,
+        "longitude": 121.529171
+    },
+    {
+        "name": "立裴米緹咖啡館",
+        "address": "台北市中山北路二段16巷10號",
+        "phone": "02 2531 0189",
+        "openTime": "12:00–22:00",
+        "latitude": 25.053022,
+        "longitude": 121.521677
+    },
+    {
+        "name": "布拉格",
+        "address": "北市泰順街40巷30號",
+        "phone": "02 2369-7722",
+        "openTime": "14:00~24:00 (每週二公休)",
+        "latitude": 25.0242249,
+        "longitude": 121.529913
+    },
+    {
+        "name": "COZY",
+        "address": "台北市大安區麗水街33巷23號",
+        "phone": "02-23974754",
+        "openTime": "",
+        "latitude": 25.028039,
+        "longitude": 121.530065
+    },
+    {
+        "name": "咖啡黑潮．Cafe Kuroshio",
+        "address": "台北市和平東路1段141巷9-1號",
+        "phone": "(02)23570018",
+        "openTime": "星期一公休",
+        "updateTime": "2013-09-27",
+        "latitude": 25.027795,
+        "longitude": 121.530646
+    },
+    {
+        "name": "熊在咖啡館",
+        "address": "新北市新莊區民安路207巷49號",
+        "phone": "02 2206 9110",
+        "openTime": "09:00-21:00",
+        "updateTime": "2014-08-14",
+        "latitude": 25.0242353,
+        "longitude": 121.4278479
+    },
+    {
+        "name": "CO2",
+        "address": "北市新生南路3段86巷11號",
+        "phone": "02-23621358",
+        "openTime": "",
+        "latitude": 25.018813,
+        "longitude": 121.532223
+    },
+    {
+        "name": "帕多瓦咖啡",
+        "address": "北市羅斯福路3段283巷14弄16號",
+        "phone": "02-23637145",
+        "openTime": "",
+        "latitude": 25.018351,
+        "longitude": 121.532659
+    },
+    {
+        "name": "統將專業咖啡",
+        "address": "北市溫州街74巷1號",
+        "phone": "02-23661594",
+        "openTime": "",
+        "latitude": 25.0197392,
+        "longitude": 121.5326626
+    },
+    {
+        "name": "希臘左巴",
+        "address": "北市羅斯福路三段283巷7弄16號",
+        "phone": "02-23633925",
+        "openTime": "",
+        "latitude": 25.0188257,
+        "longitude": 121.5310356
+    },
+    {
+        "name": "朱利安諾咖啡",
+        "address": "北市溫州街74巷一弄2號1樓",
+        "phone": "02-23660816",
+        "openTime": "",
+        "latitude": 25.0195105,
+        "longitude": 121.5326347
+    },
+    {
+        "name": "聊聊咖啡館",
+        "address": "北市溫州街87號",
+        "phone": "02-23694717",
+        "openTime": "",
+        "latitude": 25.019136,
+        "longitude": 121.53294
+    },
+    {
+        "name": "TU-coffee",
+        "address": "北市溫州街91號1樓",
+        "phone": "02-23694905",
+        "openTime": "",
+        "latitude": 25.018902,
+        "longitude": 121.532915
+    },
+    {
+        "name": "羅曼義式咖啡",
+        "address": "北市新生南路3段76巷7號",
+        "phone": "02-23623787",
+        "openTime": "",
+        "latitude": 25.019132,
+        "longitude": 121.533196
+    },
+    {
+        "name": "魯米耶",
+        "address": "北市羅斯福路三段283巷14弄15號",
+        "phone": "02-23643440",
+        "openTime": "",
+        "latitude": 25.0184769,
+        "longitude": 121.532639
+    },
+    {
+        "name": "葉子",
+        "address": "北市羅斯福路三段283巷14弄16-1號",
+        "phone": "02-23634943",
+        "openTime": "",
+        "latitude": 25.018359,
+        "longitude": 121.532683
+    },
+    {
+        "name": "雪可屋",
+        "address": "北市溫州街86號",
+        "phone": "02-23673036",
+        "openTime": "",
+        "latitude": 25.018856,
+        "longitude": 121.5328616
+    },
+    {
+        "name": "挪威森林-溫州店",
+        "address": "北市溫州街82號",
+        "phone": "02-23633714",
+        "openTime": "",
+        "latitude": 25.0190909,
+        "longitude": 121.532742
+    },
+    {
+        "name": "挪威森林-公館店",
+        "address": "北市羅斯福路三段284巷9號",
+        "phone": "02-23653089",
+        "openTime": ""
+    },
+    {
+        "name": "傑克魔豆",
+        "address": "北市羅斯福路三段316巷16號2F",
+        "phone": "02-23694893",
+        "openTime": ""
+    },
+    {
+        "name": "老樹咖啡",
+        "address": "台北市新生南路一段60號",
+        "phone": "02 2351 6452",
+        "openTime": "10:00–00:00"
+    },
+    {
+        "name": "夏洛特咖啡",
+        "address": "台北市仁愛路三段96號",
+        "phone": "02 2706 8134",
+        "openTime": "10:30~22:30",
+        "latitude": 25.037544,
+        "longitude": 121.539283
+    },
+    {
+        "name": "Aura微光咖啡",
+        "address": "台北市大安區羅斯福路三段269巷9號",
+        "phone": "02 8369 3577",
+        "openTime": "12:00–23:00"
+    },
+    {
+        "name": "道南館",
+        "address": "台北市文山區新光路一段8號",
+        "phone": "0918-626-131",
+        "openTime": "週一 – 週六/ 11:00 – 21:00, 週日/11:00 – 18:00",
+        "latitude": 24.988677,
+        "longitude": 121.57352
+    },
+    {
+        "name": "The Lobby of Simple Kaffa",
+        "address": "台北市大安區敦化南路一段177巷48號B1",
+        "phone": "02-8771-1127",
+        "openTime": "12:30~21:30",
+        "latitude": 25.0427559,
+        "longitude": 121.551656
+    },
+    {
+        "name": "Akuma caca 可可設計人文咖啡",
+        "address": "台北市大安區四維路14巷6號B1",
+        "phone": "02-27019227",
+        "openTime": "週一 ~週四、週日：12:00-22:00 週五~週六：12:00-23:00, 每月第一個禮拜二為公休日"
+    },
+    {
+        "name": "AKUMA CACA II 敦南店",
+        "address": "台北市大安區敦化南路二段218號",
+        "phone": "02 2739 2150",
+        "openTime": "週一 - 週五: 8:30-17:30"
+    },
+    {
+        "name": "沛洛瑟自家焙煎咖啡店",
+        "address": "台北市中正區中華路二段75巷40號",
+        "phone": "02-23122955",
+        "openTime": "10:00–21:00"
+    },
+    {
+        "name": "RUFOUS COFFEE",
+        "address": "復興南路二段333號",
+        "phone": "02 2736 6880",
+        "openTime": ""
+    },
+    {
+        "name": "聞山自家焙煎咖啡館",
+        "address": "文山區景中街19號",
+        "phone": "02 2933 4567",
+        "openTime": "週一 - 週日: 13:00 - 22:00",
+        "latitude": 24.993038,
+        "longitude": 121.542156
+    },
+    {
+        "name": "GEORGE HOUSE台北永康門市",
+        "address": "台北市永康街75巷3號",
+        "phone": "02-23920801",
+        "openTime": "13:00~23:00"
+    },
+    {
+        "name": "原豆空間自家烘焙咖啡館",
+        "address": "北市忠孝東路553巷6弄2號",
+        "phone": "02 2749 5027",
+        "openTime": "週一 - 週五: 11:00 - 22:00, 週六: 11:00 - 0:00, 週日: 11:00 - 21:00",
+        "latitude": 22.9798091,
+        "longitude": 120.224982
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
+        "name": "",
+        "address": "",
+        "phone": "",
+        "openTime": ""
+    },
+    {
         "name": "",
         "address": "",
         "phone": "",
